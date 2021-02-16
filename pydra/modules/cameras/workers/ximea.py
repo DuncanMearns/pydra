cb6dabfcd5c08ad0e1f62a939f66ab364109128d
duncan_mearns <mearns@neuro.mpg.de>
2021-02-16 21:00:59 +0100

    Update camera modules

--- pydra/modules/cameras/workers/ximea.py
+++ pydra/modules/cameras/workers/ximea.py
@@ -1,3 +1,9 @@
+try:
+    from ximea import xiapi
+except NameError:
+    raise Exception(
+        "The xiapi package must be installed to use a Ximea camera!"
+    )
 from pydra.modules.cameras.workers._base import CameraAcquisition
 
 
@@ -8,19 +14,12 @@ class XimeaCamera(CameraAcquisition):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.id = 0
+        self.frame = None
 
     def setup(self):
-        """ """
-        try:
-            from pydra.modules.cameras.camera_workers.ximea import xiapi
-            self.camera = xiapi.Camera(self.id)
-        except NameError:
-            raise Exception(
-                "The xiapi package must be installed to use a Ximea camera!"
-            )
-
+        self.camera = xiapi.Camera(self.id)
         self.camera.open_device()
-        self.im = xiapi.Image()
+        self.frame = xiapi.Image()
 
         # If camera supports hardware downsampling (MQ013xG-ON does,
         # MQ003MG-CM does not):
@@ -30,48 +29,56 @@ class XimeaCamera(CameraAcquisition):
 
         self.camera.start_acquisition()
         self.camera.set_acq_timing_mode("XI_ACQ_TIMING_MODE_FRAME_RATE")
+        self.set_params(
+            frame_size=self.frame_size,
+            frame_rate=self.frame_rate,
+        )
 
-    def set_frame_rate(self, fps: float):
+    def set_frame_rate(self, fps: float) -> bool:
         try:
             self.camera.set_framerate(fps)
-            return True
+            return super().set_frame_rate(fps)
         except xiapi.Xi_error:
             return False
 
-    def set_frame_size(self, width: int, height: int):
+    def set_frame_size(self, width: int, height: int) -> bool:
         try:
             self.camera.set_width(width)
             self.camera.set_height(height)
+            return super().set_frame_size(width, height)
         except xiapi.Xi_error:
             return False
 
-    def set_offsets(self, x, y):
+    def set_offsets(self, x: int, y: int) -> bool:
         try:
             self.camera.set_offsetX(x)
             self.camera.set_offsetY(y)
+            return super().set_frame_size(x, y)
         except xiapi.Xi_error:
             return False
 
-    def set_exposure(self, ms: int):
+    def set_exposure(self, u: int) -> bool:
         try:
-            self.camera.set_exposure(ms * 1000)
+            self.camera.set_exposure(u)
+            return super().set_exposure(u)
         except xiapi.Xi_error:
             return False
 
-    def set_gain(self, gain: int):
+    def set_gain(self, gain: float) -> bool:
         try:
             self.camera.set_gain(gain)
+            return super().set_gain(gain)
         except xiapi.Xi_error:
             return False
 
     def read(self):
         try:
-            self.camera.get_image(self.im)
-            frame = self.im.get_image_data_numpy()
+            self.camera.get_image(self.frame)
+            frame = self.frame.get_image_data_numpy()
         except xiapi.Xi_error:
             frame = self.empty()
         return frame
 
-    def release(self):
+    def cleanup(self):
         self.camera.stop_acquisition()
         self.camera.close_device()
